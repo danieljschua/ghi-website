b626065df608efa752da36fb173d47f162f88c72
Daniel C <chua.s.daniel@gmail.com>
2022-09-08 08:18:37 -0400

    make files

---
+++ assets.js
@@ -0,0 +1,32 @@
+officers = [
+  {
+    name: 'Mannat Srivastava',
+    role: 'President',
+    image: 'mannat.jpg',
+    year: '2024',
+  },
+  {
+    name: 'Sidhya Pathak',
+    role: 'Vice President',
+    image: 'sidhya.jpeg',
+    year: '2024',
+  },
+  {
+    name: 'Alyssa Gatesman',
+    role: 'Secretary',
+    image: 'alyssa.jpg',
+    year: '2023',
+  },
+  {
+    name: 'Sonali Behera',
+    role: 'Treasurer',
+    image: 'sonali.jpg',
+    year: '2024',
+  },
+  {
+    name: 'Daniel Chua',
+    role: 'Publicist',
+    image: 'daniel.jpg',
+    year: '2024',
+  },
+]
